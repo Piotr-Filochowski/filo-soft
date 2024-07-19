c61ce6c7107e93f9a207b5b2bdcc4d80aed020fa
Piotr Filochowski <piotr.filochowski01@gmail.com>
2024-07-19 19:44:02 +0200

    Spring initializer

---
+++ src/main/kotlin/com/filochowski/filosoft/FilosoftApplication.kt
@@ -0,0 +1,11 @@
+package com.filochowski.filosoft
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FilosoftApplication
+
+fun main(args: Array<String>) {
+	runApplication<FilosoftApplication>(*args)
+}
---
+++ src/test/kotlin/com/filochowski/filosoft/FilosoftApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.filochowski.filosoft
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FilosoftApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
